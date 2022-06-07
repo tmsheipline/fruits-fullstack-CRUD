07e87d16c8b0755a78815b4f6575625c20b2e4da
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-06-07 11:57:19 -0400

    Created partials layout page and index route

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /////////////////////////////////////////////
 // Import Our Dependencies
 /////////////////////////////////////////////
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path");
 
 /////////////////////////////////////////////
 // Database Connection
@@ -36,17 +36,19 @@ mongoose.connection
 const { Schema, model } = mongoose;
 // make fruits schema (schema = blueprint to make model)
 const fruitsSchema = new Schema({
-    name: String,
-    color: String,
-    readyToEat: Boolean,
-  });
-  // make fruit model
-  const Fruit = model("Fruit", fruitsSchema);
+  name: String,
+  color: String,
+  readyToEat: Boolean,
+});
+// make fruit model
+const Fruit = model("Fruit", fruitsSchema);
 
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
-const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+const app = require("liquid-express-views")(express(), {
+  root: [path.resolve(__dirname, "views/")],
+});
 
 /////////////////////////////////////////////////////
 // Middleware
@@ -60,12 +62,38 @@ app.use(express.static("public")); // serve files from public statically
 // Routes
 ////////////////////////////////////////////
 app.get("/", (req, res) => {
-    res.send("your server is running... better catch it.");
+  res.send("your server is running... better catch it.");
+});
+
+// Seeding the database
+app.get("/fruits/seed", (req, res) => {
+  // array of starter fruits
+  const startFruits = [
+    { name: "Orange", color: "orange", readyToEat: false },
+    { name: "Grape", color: "purple", readyToEat: false },
+    { name: "Banana", color: "orange", readyToEat: false },
+    { name: "Strawberry", color: "red", readyToEat: false },
+    { name: "Coconut", color: "brown", readyToEat: false },
+  ];
+
+  // Delete all fruits
+  Fruit.deleteMany({}).then((data) => {
+    // Seed Starter Fruits
+    Fruit.create(startFruits).then((data) => {
+      // send created fruits as response to confirm creation
+      res.json(data);
+    });
   });
-  
+});
+
+//Index Route
+app.get('/fruits', async (req,res) => {
+    const fruits = await Fruit.find({})//Fruit in caps references model
+    res.render('fruits/index.liquid', {fruits})
+})
+
 //////////////////////////////////////////////
 // Server Listener
 //////////////////////////////////////////////
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
-
