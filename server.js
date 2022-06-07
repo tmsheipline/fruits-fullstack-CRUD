b60ac2d99ada6c038b06cafa341e55474b005efd
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-06-07 11:12:24 -0400

    Added middleware and created app object

--- server.js
+++ server.js
@@ -42,3 +42,17 @@ const fruitsSchema = new Schema({
   });
   // make fruit model
   const Fruit = model("Fruit", fruitsSchema);
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
