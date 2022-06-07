20fc2035b51f106eb3e726e4a9ca0a212e89c0f4
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-06-07 10:20:38 -0400

    required dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const path = require('path');
